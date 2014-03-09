c783786ae156c5b9771541b99095296f47d30d5c
Thirunathan Sutharsan <tsuthars@gmail.com>
2014-03-09 12:08:24 +0000

    Added comments

--- URC.pl
+++ URC.pl
@@ -1,5 +1,12 @@
 #!/usr/bin/perl
 
+# Unate Recursive Complement (URC)
+# --------------------------------
+# Reads a boolean function from a file and computes
+# the inverse boolean function using URC.
+# It uses Positonal Cubic Notation (PCN) data structure to store
+# the boolean function
+
 use 5.014;
 use warnings;
 use Data::Dumper;
@@ -10,24 +17,23 @@ my $out_file = $ARGV[1];
 
 open INFILE, "$in_file";
 
-chomp(my $num_vars = <INFILE>);
-chomp(my $num_cubes = <INFILE>);
+chomp(my $num_vars = <INFILE>);  # Number of variables in the boolean function x1, x2, x3, etc.
+chomp(my $num_cubes = <INFILE>); # Number of cubes in the function which is also the number of lines rmeianing in the input file
 
-my@cube_list;
-my @dc_cube;
+my @cube_list; # Input cube list
+my @dc_cube;  # Create a dont-care cube list with appropriate number of variables for re-use
 for my $i (0..($num_vars-1)) {
-  push @dc_cube, 3;
+  push @dc_cube, 3;  # 3 or "11" represents dont-care value
 }
 
-my @cube_list;
 for my $i (0..($num_cubes-1)) {
   my $cube_line = <INFILE>;
   my @cube_vals = split /\s+/, $cube_line;
-  my $num_cares = shift @cube_vals;
-  my @cube = @dc_cube;
+  my $num_cares = shift @cube_vals; # Get the number of non-dont-care vlaues (ie. care values)
+  my @cube = @dc_cube;              # Init with the dont-care cube
   for my $j (@cube_vals) {
-    if ($j>0) {
-      $cube[$j-1] = 1;
+    if ($j>0) {                     # Greater than 0 means x
+      $cube[$j-1] = 1;              # which is represented by
     } else {
       $cube[-$j-1] = 2;
     }
